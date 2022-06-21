c6d695ad4a285d218663f2e0d1aeef161ffa434a
SebasL07 <sebasgnv0207@gmail.com>
2022-06-21 07:20:05 -0500

    some doc added

--- src/model/Concessionaire.java
+++ src/model/Concessionaire.java
@@ -2,19 +2,55 @@ package model;
 import java.util.ArrayList;
 
 public class Concessionaire{
-	
+	/**
+	 * SIZE_I is te constant size of the parking representing the rows
+	 * */
 	protected static final int SIZE_I = 10;
+	/**
+	 * SIZE_J is te constant size of the parking representing the columns
+	 * */
 	protected static final int SIZE_J = 5;
 
+	/**
+	 * vehicles is an arraylist of objects type vehicles 
+	 * */
 	protected ArrayList<Vehicle> vehicles;
+	/**
+	 * parking is a matrix of objects vehicle that represents a parking in real life
+	 * */
 	protected Vehicle[][] parking;
 
-
+	/**
+	 * Constructor of the class Concessionaire
+	 * pos: initializes vehicles and parking
+	 * */
 	public Concessionaire(){
 		vehicles = new ArrayList<Vehicle>();
 		parking = new Vehicle[SIZE_I][SIZE_J];
 	}
 
+	/**
+	 * This method is for creating a new object of the class GasCar
+	 * @param basePrice double,  is the base price of the vehicle
+	 * @param brand String, is the brand of the vehicle
+	 * @param model int, is the year of the vehicle(model)
+	 * @param cylinderCapacity double, is the cylinder capacity of the vehicle
+	 * @param mileage double, is  the mileage of the vehicle
+	 * @param wasUsed int, is a number to do another process and stablish the state of the vehicle
+	 * @param licensePlate String, is the licensePlate of the vehicle
+	 * @param numDoors int, is the number of doors that have the car
+	 * @param tintedWindows int, is a number to do another process and stablish if the car has tinted windows
+	 * @param type int, is a number to start another process to stablish the type of car it is 
+	 * @param tankCapacity double, is the capacity of the gas tank of the car
+	 * @param typeG int, is a number to start another process to stablish the type of gas that the car uses
+	 * @param priceSoat double, is the price of the soat if the vehicle has it
+	 * @param priceMT double, is the price of the technical-mechanical review
+	 * @param price double, is the price of the property card if the vehicle has it  
+	 * @param yearSoat int, is the year of the soat if the vehicle has it
+	 * @param yearMT int, is the year of the technical-mechanical review
+	 * @param year int, is the year of the property card
+	 * @param insurance double, is the insurance that the 
+	 */ 
 	public void addGasCar(double basePrice, String brand, int model, double cylinderCapacity, double mileage, int wasUsed, String licensePlate,int numDoors, int tintedWindows, int type, double tankCapacity, int typeG,double priceSoat,double priceMT,double price, int yearSoat, int yearMT,int year, double insurance, double gasLevel){
 
 		boolean used = false;
